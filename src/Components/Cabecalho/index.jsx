a9bace63ce1108b66131ce18d059d977d2abb46a
francis.lovo <francis.lovo@externo.cresol.coop.br>
2023-03-18 18:08:44 -0300

    add props ao styled

--- src/Components/Cabecalho/index.jsx
+++ src/Components/Cabecalho/index.jsx
@@ -1,19 +1,43 @@
 import React from "react";
 import bank_logo from "../../assets/images/bank_logo.svg";
+import styled from "styled-components";
+import { corPrimaria } from "../UI/variaveis";
+
+const StyledHeader = styled.nav`
+  background-color: ${corPrimaria};
+  display: flex;
+  justify-content: space-between;
+  padding: 0 15vw;
+  height: 10vh;
+  align-items: center;
+`;
+const Logo = styled.img`
+  height: 50px;
+  width: 50px;
+`;
+
+const BtnCabecalho = styled.a`
+  text-align: center;
+  border-radius: 3px;
+  padding: 5px 20px;
+  margin: 0 10px;
+  font-weight: 600;
+  border: 2px solid white;
+  background: ${(props) => (props.primary ? "transparent" : "white")};
+  color: ${(props) => (props.primary ? "white" : corPrimaria)};
+`;
 
 const Cabecalho = () => {
   return (
-    <div className="cabecalho">
-      <img className="imagem-logo" src={bank_logo} alt="Logo Smart Bank" />
+    <StyledHeader>
+      <Logo src={bank_logo} alt="Logo Smart Bank" />
       <div>
-        <a className="btn-secundario" href="https://google.com">
-          Ajuda
-        </a>
-        <a className="btn-primario" href="https://google.com">
+        <BtnCabecalho href="https://google.com">Ajuda</BtnCabecalho>
+        <BtnCabecalho href="https://google.com" primary>
           Sair
-        </a>
+        </BtnCabecalho>
       </div>
-    </div>
+    </StyledHeader>
   );
 };
 
